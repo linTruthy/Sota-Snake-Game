cbd8d6a3f0b557d57cb3877c4823c89105c81a14
liebefreundin92 <liebefreundin92@gmail.com>
2026-01-07 16:17:27 +0000

    vde

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -56,10 +56,10 @@
         #   command = ["flutter" "run" "--machine" "-d" "web-server" "--web-hostname" "0.0.0.0" "--web-port" "$PORT"];
         #   manager = "flutter";
         # };
-        # android = {
-        #   command = [ "flutter" "run" "--machine" "-d" "android" "-d" "emulator-5554" ];
-        #   manager = "flutter";
-        # };
+        android = {
+          command = [ "flutter" "run" "--machine" "-d" "android" "-d" "emulator-5554" ];
+          manager = "flutter";
+        };
       };
     };
   };
---
+++ android/app/src/main/kotlin/com/truthysystems/sota/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.truthysystems.sota
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
