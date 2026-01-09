a17b83176432418162221db6138378c1877a54ff
liebefreundin92 <liebefreundin92@gmail.com>
2026-01-09 07:31:40 +0000

    seerrrf

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") apply false
     id("org.jetbrains.kotlin.android") apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
 
 allprojects {
